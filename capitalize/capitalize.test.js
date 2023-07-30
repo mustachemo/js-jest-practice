9b440dcd2b3ca28f73f696f281c5c8ed209ebcf7
moproblems8 <mhasan1080@gmail.com>
2023-07-30 14:40:04 -0700

    extra test for practice

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -2,4 +2,5 @@ import capitalize from './capitalize';
 
 test('capitalize first letter of string', () => {
 	expect(capitalize('mohamed')).toBe('Mohamed');
+	expect(capitalize('mohamed')).not.toBe('mohamed');
 });
