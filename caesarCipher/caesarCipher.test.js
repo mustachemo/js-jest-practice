cfe32e0f7b1412827ae7b1ac5de818204a465dd1
moproblems8 <mhasan1080@gmail.com>
2023-07-30 16:54:55 -0700

    made caeserCipher tests more descriptive

--- caesarCipher/caesarCipher.test.js
+++ caesarCipher/caesarCipher.test.js
@@ -1,42 +1,37 @@
 import caesarCipher from './caesarCipher';
 
-test('caesarCipher', () => {
-	expect(caesarCipher('A', 1)).toBe('B');
-	expect(caesarCipher('A', 26)).toBe('A');
-	expect(caesarCipher('A', 27)).toBe('B');
-	expect(caesarCipher('A', -1)).toBe('Z');
-	expect(caesarCipher('A', -26)).toBe('A');
-	expect(caesarCipher('A', -27)).toBe('Z');
+describe('caesarCipher', () => {
+	it('should rotate a single letter forward and backwords', () => {
+		expect(caesarCipher('A', 1)).toBe('B');
+		expect(caesarCipher('A', -1)).toBe('Z');
+		expect(caesarCipher('Z', 26)).toBe('Z');
+		expect(caesarCipher('Z', -26)).toBe('Z');
+		expect(caesarCipher('a', 1)).toBe('b');
+		expect(caesarCipher('a', -1)).toBe('z');
+		expect(caesarCipher('z', 26)).toBe('z');
+		expect(caesarCipher('z', -26)).toBe('z');
+	});
 
-	expect(caesarCipher('Z', 1)).toBe('A');
-	expect(caesarCipher('Z', 26)).toBe('Z');
-	expect(caesarCipher('Z', 27)).toBe('A');
-	expect(caesarCipher('Z', -1)).toBe('Y');
-	expect(caesarCipher('Z', -26)).toBe('Z');
-	expect(caesarCipher('Z', -27)).toBe('Y');
+	it('should rotate a word forward and backwords', () => {
+		expect(caesarCipher('Hello', 1)).toBe('Ifmmp');
+		expect(caesarCipher('Hello', -1)).toBe('Gdkkn');
+		expect(caesarCipher('Hello', 26)).toBe('Hello');
+		expect(caesarCipher('Hello', -26)).toBe('Hello');
+		expect(caesarCipher('hello', 1)).toBe('ifmmp');
+		expect(caesarCipher('hello', -1)).toBe('gdkkn');
+		expect(caesarCipher('hello', 26)).toBe('hello');
+		expect(caesarCipher('hello', -26)).toBe('hello');
+	});
 
-	expect(caesarCipher('a', 1)).toBe('b');
-	expect(caesarCipher('a', 26)).toBe('a');
-	expect(caesarCipher('a', 27)).toBe('b');
-	expect(caesarCipher('a', -1)).toBe('z');
-	expect(caesarCipher('a', -26)).toBe('a');
-	expect(caesarCipher('a', -27)).toBe('z');
+	it('should rotate a sentence forward and backwords', () => {
+		expect(caesarCipher('Hello, World!', 1)).toBe('Ifmmp, Xpsme!');
+		expect(caesarCipher('Hello, World!', 26)).toBe('Hello, World!');
+		expect(caesarCipher('Hello, World!', 27)).toBe('Ifmmp, Xpsme!');
+	});
 
-	expect(caesarCipher('z', 1)).toBe('a');
-	expect(caesarCipher('z', 26)).toBe('z');
-	expect(caesarCipher('z', 27)).toBe('a');
-	expect(caesarCipher('z', -1)).toBe('y');
-	expect(caesarCipher('z', -26)).toBe('z');
-	expect(caesarCipher('z', -27)).toBe('y');
-
-	expect(caesarCipher('Hello, World!', 1)).toBe('Ifmmp, Xpsme!');
-	expect(caesarCipher('Hello, World!', 26)).toBe('Hello, World!');
-	expect(caesarCipher('Hello, World!', 27)).toBe('Ifmmp, Xpsme!');
-	expect(caesarCipher('Hello, World!', -1)).toBe('Gdkkn, Vnqkc!');
-	expect(caesarCipher('Hello, World!', -26)).toBe('Hello, World!');
-	expect(caesarCipher('Hello, World!', -27)).toBe('Gdkkn, Vnqkc!');
-
-	expect(() => caesarCipher(1, 1)).toThrow();
-	expect(() => caesarCipher('A', 'A')).toThrow();
-	expect(() => caesarCipher(1, 'A')).toThrow();
+	it('should throw errors', () => {
+		expect(() => caesarCipher(1, 1)).toThrow();
+		expect(() => caesarCipher('A', 'A')).toThrow();
+		expect(() => caesarCipher(1, 'A')).toThrow();
+	});
 });
