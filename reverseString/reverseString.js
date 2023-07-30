092d2ec55455850711a167c81bd5ceb4088650c2
moproblems8 <mhasan1080@gmail.com>
2023-07-30 14:56:40 -0700

    added more complexity to tests with throw error

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
 // a function to reverse a string
 
 export default function reverseString(inputString) {
+	if (typeof inputString !== 'string') {
+		throw new Error('Input must be a string');
+	}
 	return inputString.split('').reverse().join('');
 }
--- reverseString/reverseString.test.js
+++ reverseString/reverseString.test.js
@@ -9,3 +9,11 @@ describe('reverseString', () => {
 test('reverseString', () => {
 	expect(reverseString('mohamed')).toEqual('demahom');
 });
+
+test('should throw error for invalid input', () => {
+	expect(() => reverseString(123)).toThrow('Input must be a string');
+});
+
+test('should handle empty string', () => {
+	expect(reverseString('')).toEqual('');
+});
